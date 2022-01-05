3dd530da9532fc21a7d83af1d679fa6c09cac6e3
Ryan <jesselockard99@gmail.com>
2022-01-05 01:53:45 -0500

    added default command to args, added support for positional arguments

--- src/tasks/args.ts
+++ src/tasks/args.ts
@@ -1,5 +1,5 @@
 import { cwd } from "process";
-import yargs from "yargs";
+import Yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 import { RWDeps } from "./prompt.js";
@@ -10,7 +10,7 @@ import { RWDeps } from "./prompt.js";
  * Gathers arguments from the command-line, given when the script is executed.
  */
 
-const y = yargs(hideBin(process.argv));
+const y = Yargs(hideBin(process.argv));
 
 export interface ArgsResponse {
   name?: string;
@@ -40,51 +40,59 @@ export const getArgs = async ({ scriptName, version }: ArgsConfig) =>
     )
     .version(version)
     .help()
-    .positional("name", {
-      type: "string",
-      desc: "The name of your NPM package.",
-    })
-    .positional("path", {
-      type: "string",
-      desc: "A path to the directory your package will be generated in.",
-      default: cwd(),
-    })
-    .option("dry", {
-      type: "boolean",
-      desc: "Run this script, skipping generation.",
-      default: false,
-    })
-    .option("force", {
-      type: "boolean",
-      desc: "Overwrite the output directory if it already exists.",
-      default: false,
-    })
-    .option("skipPrompts", {
-      type: "boolean",
-      desc: "Completely skip prompts, passing along only arguments to the generator.",
-      default: false,
-    })
-    .option("lang", {
-      type: "string",
-      desc: "The language to generate your package in.",
-      choices: ["ts", "js"],
-    })
-    .option("rwDeps", {
-      type: "array",
-      desc: "RedwoodJS packages to include.",
-      choices: RWDeps,
-    })
-    .option("react", {
-      type: "boolean",
-      desc: "Include React as a dependency.",
-    })
-    .option("storybook", {
-      type: "boolean",
-      desc: "Setup Storybook in your package.",
-    })
-    .option("cli", {
-      type: "boolean",
-      desc: "Add dependencies for building interactive CLI tools.",
-      hidden: true, // TODO: Temporary - pending implementation
-    })
-    .strict().argv) as ArgsResponse;
+    .command(
+      "* [name] [path]",
+      "Generates an NPM package tailored to RedwoodJS applications.",
+      (yargs) => {
+        yargs
+          .positional("name", {
+            type: "string",
+            desc: "The name of your NPM package.",
+          })
+          .positional("path", {
+            type: "string",
+            desc: "A path to the directory your package will be generated in.",
+            default: cwd(),
+          })
+          .option("dry", {
+            type: "boolean",
+            desc: "Run this script, skipping generation.",
+            default: false,
+          })
+          .option("force", {
+            type: "boolean",
+            desc: "Overwrite the output directory if it already exists.",
+            default: false,
+          })
+          .option("skipPrompts", {
+            type: "boolean",
+            desc: "Completely skip prompts, passing along only arguments to the generator.",
+            default: false,
+          })
+          .option("lang", {
+            type: "string",
+            desc: "The language to generate your package in.",
+            choices: ["ts", "js"],
+          })
+          .option("rwDeps", {
+            type: "array",
+            desc: "RedwoodJS packages to include.",
+            choices: RWDeps,
+          })
+          .option("react", {
+            type: "boolean",
+            desc: "Include React as a dependency.",
+          })
+          .option("storybook", {
+            type: "boolean",
+            desc: "Setup Storybook in your package.",
+            implies: "react",
+          })
+          .option("cli", {
+            type: "boolean",
+            desc: "Add dependencies for building interactive CLI tools.",
+            hidden: true, // TODO: Temporary - pending implementation
+          });
+      }
+    )
+    .strict().argv) as unknown as ArgsResponse;
